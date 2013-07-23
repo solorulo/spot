e375c4f8945d68e4cc85540e3c239b7a43b4a947
Rulo <solorulo@hotmail.com>
2013-07-23 02:16:50 -0500

    params

--- spot_app/views.py
+++ spot_app/views.py
@@ -32,7 +32,18 @@ def handler500(request):
 	}
 	data = simplejson.dumps(_json)
 	return HttpResponse(data)
-
+def csrf_exempt(view_func):
+        """
+        Marks a view function as being exempt from the CSRF view protection.
+        """
+        # We could just do view_func.csrf_exempt = True, but decorators
+        # are nicer if they don't have side-effects, so we return a new
+        # function.
+        def wrapped_view(request,*args, **kwargs):
+            return view_func(request, *args, **kwargs)
+            if request.META.has_key('SpotStreet-X-Key'):
+                wrapped_view.csrf_exempt = True
+        return wraps(view_func, assigned=available_attrs(view_func))(wrapped_view)
 def url(self, **options):
     options.update(format = self.format, version = self.version)
     return utils.cloudinary_url(self.public_id, **options)[0]
