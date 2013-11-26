7b5410333fd745c12e1f9036a84e3cbe6767c6b4
Rulo <solorulo@hotmail.com>
2013-11-26 02:58:19 -0600

    photo add fix usernewphoto debug

--- spot_app/photo_views.py
+++ spot_app/photo_views.py
@@ -59,7 +59,7 @@ def photo_add(request):
 
 	new_foto = Foto(
 		foto_url=foto_id, 
-		user=request.user, 
+		user=request.user.pk, 
 		fecha=current_datetime,
 		delay=delayVal,
 		descripcion=desc,
